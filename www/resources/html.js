ad12ec6cf68959b947394b1d9aaa20b2308b3823
Sondre Sallaup <sondre.sallaup@gmail.com>
2014-06-08 22:31:16 +0200

    initial commit

---
+++ www/resources/html.js
@@ -0,0 +1,6 @@
+var HTML_HEAD= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" content="text/html; charset=UTF-8"><link href = "/css/ionic.css" rel = "stylesheet">';
+
+var HTML_TITLE_DESIGN = '<div class="bar bar-header bar-royal">';
+
+var HTML_CARD_HEADER = '<div class="card"><div class="item item-divider">';
+var HTML_CARD_BODY = '</div><div class="item item-text-wrap">';
---
+++ www/resources/strings.js
@@ -0,0 +1,10 @@
+var APP_NAME = "20 spørsmål";
+
+var BACK = "Tilbake";
+
+var YOUR_TURN = "Din tur";
+var NEW_GAME = "Nytt spill";
+var WAITING_GAME = "Venter på spiller";
+
+var NEW_GAME_FRIEND = "Venn";
+var NEW_GAME_RANDOM = "Tilfeldig";
